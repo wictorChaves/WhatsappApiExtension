c1c99021ccf3e54fb2441556983fbbcec60056cb
Wictor Chaves <wictor@hotmail.com.br>
2020-07-05 10:52:32 -0300

    Update store-create.js

--- js/web_accessible_resources/store-create.js
+++ js/web_accessible_resources/store-create.js
@@ -1,61 +1,89 @@
-"use strict";
+function createStore() {
+    if (!window.Store) {
+        (function () {
+            function getStore(modules) {
+                let foundCount = 0;
+                let neededObjects = [
+                    { id: "Store", conditions: (module) => (module.default && module.default.Chat && module.default.Msg) ? module.default : null },
+                    { id: "MediaCollection", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.processAttachments) ? module.default : null },
+                    { id: "MediaProcess", conditions: (module) => (module.BLOB) ? module : null },
+                    { id: "Wap", conditions: (module) => (module.createGroup) ? module : null },
+                    { id: "ServiceWorker", conditions: (module) => (module.default && module.default.killServiceWorker) ? module : null },
+                    { id: "State", conditions: (module) => (module.STATE && module.STREAM) ? module : null },
+                    { id: "WapDelete", conditions: (module) => (module.sendConversationDelete && module.sendConversationDelete.length == 2) ? module : null },
+                    { id: "Conn", conditions: (module) => (module.default && module.default.ref && module.default.refTTL) ? module.default : null },
+                    { id: "WapQuery", conditions: (module) => (module.queryExist) ? module : ((module.default && module.default.queryExist) ? module.default : null) },
+                    { id: "CryptoLib", conditions: (module) => (module.decryptE2EMedia) ? module : null },
+                    { id: "OpenChat", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.openChat) ? module.default : null },
+                    { id: "UserConstructor", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.isServer && module.default.prototype.isUser) ? module.default : null },
+                    { id: "SendTextMsgToChat", conditions: (module) => (module.sendTextMsgToChat) ? module.sendTextMsgToChat : null },
+                    { id: "SendSeen", conditions: (module) => (module.sendSeen) ? module.sendSeen : null },
+                    { id: "sendDelete", conditions: (module) => (module.sendDelete) ? module.sendDelete : null }
+                ];
+                for (let idx in modules) {
+                    if ((typeof modules[idx] === "object") && (modules[idx] !== null)) {
+                        let first = Object.values(modules[idx])[0];
+                        if ((typeof first === "object") && (first.exports)) {
+                            for (let idx2 in modules[idx]) {
+                                let module = modules(idx2);
+                                if (!module) {
+                                    continue;
+                                }
+                                neededObjects.forEach((needObj) => {
+                                    if (!needObj.conditions || needObj.foundedModule)
+                                        return;
+                                    let neededModule = needObj.conditions(module);
+                                    if (neededModule !== null) {
+                                        foundCount++;
+                                        needObj.foundedModule = neededModule;
+                                    }
+                                });
+                                if (foundCount == neededObjects.length) {
+                                    break;
+                                }
+                            }
 
-function _defineProperty(obj, key, value) {
-    if (key in obj)
-    {
-        Object.defineProperty(obj, key, {
-            value: value,
-            enumerable: true,
-            configurable: true,
-            writable: true
-        });
-    }
-    else
-    {
-        obj[key] = value;
-    }
-    return obj;
-}
-
-setTimeout(function () {
-    function getAllModules() {
-        return new Promise(function (resolve) {
-            var id = _.uniqueId("fakeModule_");
-            window["webpackJsonp"]([], _defineProperty({}, id, function (module, exports, __webpack_require__) {
-                resolve(__webpack_require__.c);
-            }), [id]);
-        });
-    }
-
-    var modules = getAllModules()._value;
-
-    //  Automatically locate modules
-   
-    for (var key in modules) {
-        if (modules[key].exports) {
-            if (modules[key].exports.default) {
-                if (modules[key].exports.default.Wap) {
-                    store_id = modules[key].i.replace(/"/g, '"');                    
+                            let neededStore = neededObjects.find((needObj) => needObj.id === "Store");
+                            window.Store = neededStore.foundedModule ? neededStore.foundedModule : {};
+                            neededObjects.splice(neededObjects.indexOf(neededStore), 1);
+                            neededObjects.forEach((needObj) => {
+                                if (needObj.foundedModule) {
+                                    window.Store[needObj.id] = needObj.foundedModule;
+                                }
+                            });
+                            window.Store.sendMessage = function (e) {
+                                return window.Store.SendTextMsgToChat(this, ...arguments);
+                            };
+                            return window.Store;
+                        }
+                    }
                 }
             }
-        }
+
+            if (typeof webpackJsonp === 'function') {
+                webpackJsonp([], { 'parasite': (x, y, z) => getStore(z) }, ['parasite']);
+            } else {
+                webpackJsonp.push([
+                    ['parasite'],
+                    {
+                        parasite: function (o, e, t) {
+                            getStore(t);
+                        }
+                    },
+                    [['parasite']]
+                ]);
+            }
+
+        })();
     }
-}, 5000);
-
-function _requireById(id) {
-    return webpackJsonp([], null, [id]);
 }
 
-// Module IDs
-var store_id = 0;
-var Store = {};
-
-function init() {
-    Store = _requireById(store_id).default;
+console.log("Store Create");
+console.log("Waiting...");
+setTimeout(function(){
+    console.log("Created");
+    createStore();
     console.log(Store);
+    console.log("Start loop")
     loop();
-}
-
-setTimeout(function () {
-    init();
 }, 7000);
